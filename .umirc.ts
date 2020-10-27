5b423db9b8dea3ed41ea07f0fd56e0907b8cc7bd
zexuan.zzx <zexuan.zzx@alibaba-inc.com>
2020-10-27 20:19:04 +0800

    dev

--- .umirc.ts
+++ .umirc.ts
@@ -7,4 +7,8 @@ export default {
     title: 'packjs',
     logo: 'https://gw.alicdn.com/tfs/TB1ILY9YUH1gK0jSZSyXXXtlpXa-742-996.png',
     favicon: 'https://gw.alicdn.com/tfs/TB1ILY9YUH1gK0jSZSyXXXtlpXa-742-996.png',
+    navs: {
+        'en-US': [null, { title: 'GitHub', path: 'https://github.com/tageecc/packjs' }],
+        'zh-CN': [null, { title: 'GitHub', path: 'https://github.com/tageecc/packjs' }],
+    },
 };
