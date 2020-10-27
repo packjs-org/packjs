b5e18a6955f30e8637f4b9ab58818425ce172f88
zexuan.zzx <zexuan.zzx@alibaba-inc.com>
2020-10-27 16:41:27 +0800

    dev

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,8 @@
 export default {
-    outputPath: '../docs',
+    base: '/packjs',
     publicPath: `/packjs/`,
-    base: '/packjs/',
+    exportStatic: {},
+    dynamicImport: {},
     mode: 'site',
     title: 'packjs',
     logo: 'https://gw.alicdn.com/tfs/TB1ILY9YUH1gK0jSZSyXXXtlpXa-742-996.png',
