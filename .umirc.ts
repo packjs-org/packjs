07546fd3096d2629c7a492d5873c078b0b9d0b26
zexuan.zzx <zexuan.zzx@alibaba-inc.com>
2020-10-27 20:42:09 +0800

    dev

--- .umirc.ts
+++ .umirc.ts
@@ -13,7 +13,7 @@ export default {
         'en-US': [null, { title: 'GitHub', path: HOME_PATH }],
         'zh-CN': [null, { title: 'GitHub', path: HOME_PATH }],
     },
-    plugins: ['analytics'],
+    plugins: ['@umijs/plugin-analytics'],
     analytics: {
         ga: 'G-ZNJQM7Q9TP',
     },
